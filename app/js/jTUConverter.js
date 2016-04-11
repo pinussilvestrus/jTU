e9ce9096e7cd7a1dab17b098b4e9875f7ee13e62
Niklas Kiefer <niklas.kiefer@student.hpi.uni-potsdam.de>
2016-04-11 07:32:18 +0200

    improve

--- app/js/jTUConverter.js
+++ app/js/jTUConverter.js
@@ -4,11 +4,50 @@ var _converter = {};
 * ##structure of umlObject##
 {
   className: String,
-  attributes: Array of {name: String, type: String of ['string', 'number', 'date', 'Object']},
+  attributes: Array of {name: String, type: String of ['string', 'number', 'date', 'Object', List of ['string', 'number', List, 'object']]},
   associations: Array of umlObjects
 }
-*
+* todo: if array has just equal-typed objects, then add to associations
 **/
+
+var _isArray = function(json) {
+  if (json.length) return true;
+  else return false;
+};
+
+var _typeOf = function(json) {
+  if (typeof(json) === 'object') {
+    if (_isArray(json)) {
+      return 'Array<' + _getTypeOfArray(json) + '>';
+    } else {
+      var _object = _converter.jsonToUML(json);
+      if (_object.className && _object.className !== '') {
+        return _object.className
+      } else {
+        return 'object';
+      }
+    }
+  }
+
+  return typeof(json);
+};
+
+var _getTypeOfArray = function(array) {
+    if (!_isArray(array)) {
+      return;
+    }
+
+    var type = _typeOf(array[0]);
+    for (var i = 1; i < array.length; i++) {
+      if (_typeOf(array[i]) !== _typeOf(array[i-1])) {
+        type = 'mixed';
+        break;
+      }
+    }
+
+    return type;
+};
+
 _converter.jsonToUML = function(json) {
   var umlObject = {
     className: '',
@@ -17,15 +56,30 @@ _converter.jsonToUML = function(json) {
   };
 
   for (key in json) {
+
+    var _property = json[key];
+
     if (key === '_umlClassName') {
-      umlObject.className = json[key];
+      umlObject.className = _property;
       continue;
     }
 
-    umlObject.attributes.push({name: key, type: typeof(json[key])});
-    if (typeof(json[key]) === 'object') {
-      umlObject.associations.push(_converter.jsonToUML(json[key]));
+    var _type = _typeOf(_property);
+    var _newAttribute = {name: key, type: _type};
+    var _object = {};
+    if (typeof(_property) === 'object') {
+      if(_type.includes('Array')) {
+        if (!_type.includes('string') && !_type.includes('number') && !_type.includes('mixed')) {
+          umlObject.associations.push(_object);
+        }
+      } else {
+        console.log('hier');
+        _object = _converter.jsonToUML(_property);
+        umlObject.associations.push(_object);
+      }
     }
+
+    umlObject.attributes.push(_newAttribute);
   }
 
   return umlObject;
--- test.js
+++ test.js
@@ -4,9 +4,11 @@ var _testJson = {
   _umlClassName: 'TestClass',
   string: "Hi",
   number: 3,
-  object: {
+  testObject: {
+    _umlClassName: 'Thing',
     test: 1
-  }
+  },
+  testArray: [{_umlClassName: 'Hello', string: 'test'}, 'string']
 };
 
 console.log(converter.jsonToUML(_testJson));
